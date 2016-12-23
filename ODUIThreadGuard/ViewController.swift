04de6125a2662aefa28d2341c48dc9abac0e7cd1
olddonkey <olddonkeyblog@gmail.com>
2016-12-23 00:37:20 -0600

    Initial Commit

---
+++ ODUIThreadGuard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ODUIThreadGuard
+//
+//  Created by olddonkey on 2016/12/23.
+//  Copyright © 2016年 Old Donkey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
